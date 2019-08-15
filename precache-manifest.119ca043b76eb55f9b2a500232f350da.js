5757dc22026847823a66297a8cc06f02ac9604ae
Baranov Igor <igor.82bv@mail.ru>
2019-08-15 23:42:39 +0300

    add: build

---
+++ precache-manifest.119ca043b76eb55f9b2a500232f350da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6eccb158b2dc56ddc8a96d3b5e1fd7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6d75a3649968868e063",
+    "url": "/static/css/main.e6e03391.chunk.css"
+  },
+  {
+    "revision": "d9553c908f071c3bcbe8",
+    "url": "/static/js/2.0ddb5acf.chunk.js"
+  },
+  {
+    "revision": "d6d75a3649968868e063",
+    "url": "/static/js/main.f5d68ef1.chunk.js"
+  },
+  {
+    "revision": "cd6183a92790f1f410ee",
+    "url": "/static/js/runtime~main.ffb341a7.js"
+  }
+]);
